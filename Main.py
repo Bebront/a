d94d743c1140fb3183e1c25b5faf8934d5941be3
Bebront <yasereda08@gmail.com>
2024-01-20 23:39:03 +0300

    теперь у нас есть еще танк

--- Main.py
+++ Main.py
@@ -20,46 +20,64 @@ def load_image(name, color_key=None):
     return image
 
 
+def if_control_key(key):
+    if key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT):
+        return 1
+    elif key in (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d):
+        return 2
+    else:
+        return 0
+
+
 class Tank(pygame.sprite.Sprite):
-    def __init__(self, radius, x, y):
+    def __init__(self, radius, x, y, rotate, keyboard):
         super().__init__(all_sprites)
         self.image = load_image('tank.png', -1)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
         self.x, self.y = x, y
-        self.speed = random.choice([10, 20, 30])
-        self.image = pygame.transform.rotate(self.image, -90)
-        self.direction = 90
-        if self.speed == 10:
-            self.bullet_speed = 300
-        elif self.speed == 20:
-            self.bullet_speed = 200
+        self.speed = random.choice([1, 2, 3])
+        self.image = pygame.transform.rotate(self.image, -rotate)
+        self.direction = rotate
+        self.rotate = rotate
+        self.keyboard = keyboard
+        if self.keyboard == 'udlr':
+            self.angles = {
+                pygame.K_UP: 0,
+                pygame.K_DOWN: 180,
+                pygame.K_RIGHT: 90,
+                pygame.K_LEFT: 270
+            }
         else:
-            self.bullet_speed = 100
+            self.angles = {
+                pygame.K_w: 0,
+                pygame.K_s: 180,
+                pygame.K_d: 90,
+                pygame.K_a: 270
+            }
+        if self.speed == 1:
+            self.bullet_speed = 30
+        elif self.speed == 2:
+            self.bullet_speed = 20
+        else:
+            self.bullet_speed = 10
 
     def update(self, direction):
-        angles = {
-            pygame.K_UP: 0,
-            pygame.K_DOWN: 180,
-            pygame.K_RIGHT: 90,
-            pygame.K_LEFT: 270
-        }
-        if not direction in angles:
+        if not direction in self.angles:
             return
-        self.image = pygame.transform.rotate(self.image, (self.direction - angles[direction]) % 360)
-        if angles[direction] == 0:
+        if self.rotate == 90:
+            self.image = pygame.transform.rotate(self.image, (self.direction - self.angles[direction]) % 360)
+        else:
+            self.image = pygame.transform.rotate(self.image, (self.direction - self.angles[direction]) % 360)
+        if self.angles[direction] == 0:
             self.rect = self.rect.move(0, -self.speed)
-        if angles[direction] == 180:
+        if self.angles[direction] == 180:
             self.rect = self.rect.move(0, self.speed)
-        if angles[direction] == 90:
+        if self.angles[direction] == 90:
             self.rect = self.rect.move(self.speed, 0)
-        if angles[direction] == 270:
+        if self.angles[direction] == 270:
             self.rect = self.rect.move(-self.speed, 0)
         print(self.y, self.x)
-        self.direction = angles[direction]
-        # if pygame.sprite.spritecollideany(self, horizontal_borders):
-        #     self.vy = -self.vy
-        # if pygame.sprite.spritecollideany(self, vertical_borders):
-        #     self.vx = -self.vx
+        self.direction = self.angles[direction]
 
 
 class Border(pygame.sprite.Sprite):
@@ -88,25 +106,36 @@ Border(5, height - 5, width - 5, height - 5)
 Border(5, 5, 5, height - 5)
 Border(width - 5, 5, width - 5, height - 5)
 
-for i in range(1):
-    Tank(20, 100, 100)
+Tank_1 = Tank(10, 100, 100, 90, 'udlr')
+Tank_2 = Tank(10, 600, 800, 270, 'wasd')
 
 clock = pygame.time.Clock()
-flag = False
-event_key = '78'
+flag_1 = False
+flag_2 = False
+event_key_2 = '78'
+event_key_1 = '56'
 running = True
 while running:
-    if flag is True:
-        all_sprites.update(event_key)
+    if flag_1 is True:
+        all_sprites.update(event_key_1)
+    if flag_2 is True:
+        all_sprites.update(event_key_2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            flag = True
-            event_key = event.key
+            if if_control_key(event.key) == 1:
+                flag_1 = True
+                event_key_1 = event.key
+            elif if_control_key(event.key) == 2:
+                flag_2 = True
+                event_key_2 = event.key
             all_sprites.update(event.key)
         elif event.type == pygame.KEYUP:
-            flag = False
+            if if_control_key(event.key) == 1:
+                flag_1 = False
+            elif if_control_key(event.key) == 2:
+                flag_2 = False
     fon = pygame.transform.scale(load_image('Fon.jpg'), size)
     screen.blit(fon, (0, 0))
     all_sprites.draw(screen)
